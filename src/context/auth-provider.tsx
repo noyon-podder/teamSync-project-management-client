124b113d7d2f90e5d418869e8548ff54a114b9dc
noyon-podder <noyon.podder7@gmail.com>
2025-04-07 20:43:02 +0600

    premission file completed

--- src/context/auth-provider.tsx
+++ src/context/auth-provider.tsx
@@ -5,11 +5,14 @@ import useAuth from "@/hooks/api/use-auth";
 import { UserType, WorkspaceType } from "@/types/api.type";
 import useGetWorkspaceQuery from "@/hooks/api/use-get-workspace";
 import { useNavigate } from "react-router-dom";
+import usePermissions from "@/hooks/use-permissions";
+import { PermissionType } from "@/constant";
 
 // Define the context shape
 type AuthContextType = {
   user?: UserType;
   workspace?: WorkspaceType;
+  hasPermission: (permission: PermissionType) => boolean;
   error: any;
   isLoading: boolean;
   workspaceLoading: boolean;
@@ -54,11 +57,18 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
     }
   }, [navigate, workspaceError]);
 
+  const permissions = usePermissions(user, workspace);
+
+  const hasPermission = (permission: PermissionType): boolean => {
+    return permissions.includes(permission);
+  };
+
   return (
     <AuthContext.Provider
       value={{
         user,
         workspace,
+        hasPermission,
         error: authError || workspaceError,
         isLoading,
         workspaceLoading,
